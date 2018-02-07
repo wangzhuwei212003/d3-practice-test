fb93aa8159a21b936f5a6bb558a8aef8ea0eada6
zhuwei wang <wangzhuwei212003@163.com>
2018-02-07 14:15:10 +0800

    Tetris demo from http://d3tetris.herokuapp.com/. But sth wrong, need to be changed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,29 +7,35 @@ import SimpleBarChart from './Tutorials/SimpleBarChart/SimpleBarChart';
 import DynamicTable from './Tutorials/DynamicTable/DynamicTable';
 import Grid from './Tutorials/MakeGrid';
 import Mower from './Tutorials/mowerDemo';
+import Tetris from './Tutorials/Tetris';
 
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-{/*
-        <BarChart data={[5,10,1,3]} size={[500, 500]} />
-*/}
-        {/*<SimpleBarChart data={[30, 86, 168, 281, 303, 365]} />*/}
-        {/*<DynamicTable/>*/}
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+// {/*
+//         <BarChart data={[5,10,1,3]} size={[500, 500]} />
+// */}
+//         {/*<SimpleBarChart data={[30, 86, 168, 281, 303, 365]} />*/}
+//         {/*<DynamicTable/>*/}
+//
+//         {/*<Grid/>*/}
+//         {/*<Mower/>*/}
+//         <Tetris/>
+//       </div>
+//     );
 
-        {/*<Grid/>*/}
-        <Mower/>
-      </div>
-    );
+    return (
+        <Tetris/>
+    )
   }
 }
 
