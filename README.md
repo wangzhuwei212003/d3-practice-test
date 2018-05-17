4f58ebbb8cfcc6cbd91c5012dcafe3f7f542b1d6
zhuwei wang <wangzhuwei212003@163.com>
2018-05-17 20:29:04 +0800

    十万火急！！！！

--- README.md
+++ README.md
@@ -2,4 +2,6 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 And this is a collection of d3 examples used in react.
 
-这个分支，
+这个分支，thesisExperiment，是供我毕业论文实验的基础！！！十万火急！
+
+其他不相干的都没有。
--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import {Menu, Icon, Button, Layout} from 'antd';
 
 import logo from './logo.svg';
 import './App.css';
-
 import 'antd/dist/antd.css';
 
 import Home from './example/Home';
@@ -18,9 +17,6 @@ import Home from './example/Home';
 import BasicAStar from './MAPF/BasicAStar';
 import Coop4 from './MAPF/Coop4';
 import Coop30 from './MAPF/Coop30';
-
-import CellVisual from './RealCell/visual';
-import OdomForShow from './OdomForShow/visual';
 
 const SubMenu = Menu.SubMenu;
 const {Header, Footer, Sider, Content} = Layout;
@@ -59,14 +55,6 @@ class App extends Component {
                     <span><Link to='/MAPF/Coop30'>Coop 30</Link></span>
                   </Menu.Item>
 
-                  <Menu.Item key="7">
-                    <Icon type="inbox"/>
-                    <span><Link to='/Cell/visual'>Real cell</Link></span>
-                  </Menu.Item>
-                  <Menu.Item key="8">
-                    <Icon type="inbox"/>
-                    <span><Link to='/OdomForShow'>用odom算位置报告超出的百分比</Link></span>
-                  </Menu.Item>
 
                 </Menu>
               </Sider>
@@ -76,10 +64,6 @@ class App extends Component {
                 <Route path="/MAPF/BasicAStar" component={BasicAStar}/>
                 <Route path="/MAPF/Coop4" component={Coop4}/>
                 <Route path="/MAPF/Coop30" component={Coop30}/>
-
-                <Route path="/Cell/visual" component={CellVisual}/>
-                <Route path="/OdomForShow" component={OdomForShow}/>
-
 
               </Content>
             </Layout>
