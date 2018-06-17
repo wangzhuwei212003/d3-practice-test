35fd022a7059c34d217a426c15654985de606a61
zhuwei wang <wangzhuwei212003@163.com>
2018-06-18 03:21:57 +0800

    添加一个 JSON2CSV 方法

--- src/MAPF/Coop4/index.js
+++ src/MAPF/Coop4/index.js
@@ -630,6 +630,73 @@ class Coop extends Component {
 
   }
 
+  // 这个是供下载成 csv Excel文件的功能。
+  // JSONData 是一个数组，里面的元素都是object，object里的键值对都是一个observation的属性。
+  // title 是一个文件名称。
+  // showLabel 是一个bool值，true标识在最终的文件中保留属性作为第一行。
+  JSON2CSV(JSONData, title, showLabel) {
+    const arrData = typeof JSONData !== 'object' ? JSON.parse(JSONData) : JSONData;
+
+    let CSV = '';
+
+    //CSV += title + '\r\n\n';
+
+    if (showLabel) {
+      let row = '';
+
+      for (let property in arrData[0]) {
+        row += property + ',';
+      }
+
+      row = row.slice(0, -1); // 最后一个逗号去掉。截取
+
+      // append label row with line break
+      CSV += row + '\r\n';
+    }
+
+    // 1st loop to extract each row
+    for (let i = 0; i < arrData.length; i += 1) {
+      let row = '';
+
+      // 2nd loop to extract each column and convert it in string comma-seprated
+      for (let index in arrData[i]) {
+        row += '"' + arrData[i][index] + '",';
+      }
+
+      row.slice(0, row.length - 1);
+
+      // add a line break after each row
+      CSV += row + '\r\n';
+    }
+
+    if (CSV === '') {
+      alert('invalid data');
+      return;
+    }
+
+    //Generate a file name
+    let fileName = 'MyReport_';
+    fileName += title.replace(/ /g, "_");
+
+    // initialize file format you want csv or xls
+    //let uri = 'data:text/csv;charset=utf-8,' + escape(CSV); // 这里为什么会提示 deprecated symbol escape ？
+    //let uri = 'data:text/csv;charset=utf-8,' + encodeURIComponent(CSV); // 这里为什么会提示 deprecated symbol escape ？
+    let uri = 'data:text/csv;charset=utf-8,' + encodeURI(CSV); // 这里为什么会提示 deprecated symbol escape ？
+
+    var link = document.createElement("a");
+    link.href = uri;
+
+    //set the visibility hidden so it will not effect on your web-layout
+    link.style = "visibility:hidden";
+    link.download = fileName + ".csv";
+
+    //this part will append the anchor tag and remove it after automatic click
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+
+  }
+
   render() {
     return (
         <div ref={ele => this.grid = ele} className="instruction">
